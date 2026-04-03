fee4e68f008fba2dec479addd57198b77bdb8ea4
altantsoojbatkhuyag <batkhuyagaltantsooj@gmail.com>
2026-04-03 12:27:02 +0800

    added cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import userRoutes from "./routes/user.routes";
 import categoryRoutes from "./routes/category.routes";
 import foodRoutes from "./routes/food.routes";
@@ -7,12 +8,20 @@ import loginRoutes from "./routes/login.routes";
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const corsOptions = {
+  origin: ["https://foodelivery-x4ac.vercel.app/", "http://localhost:4000"],
+  optionsSuccessStatus: 200, // For legacy browser support
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.use("/users", userRoutes);
 app.use("/categories", categoryRoutes);
 app.use("/foods", foodRoutes);
 app.use("/users", loginRoutes);
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
